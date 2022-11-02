20ca0ae230be41bfaa41f471638575218a1f17f0
Mason Wudtee <mason.wudtee@gmail.com>
2022-11-02 17:00:39 -0500

    this is dumb and not necessary, but at least it works?

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,7 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.ArrayList;
+
 import java.util.List;
 
 public class StudentTest {
+
+
+
+
     public static void main(String[] args) {
         Student newStudent = new Student("Mac", 15);
         newStudent.addGrade(90);
